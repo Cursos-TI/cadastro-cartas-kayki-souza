368b2fcffef563a5be3573101b1ade38a58e95f7
KAYKISON <kaikisouza988@gmail.com>
2026-07-24 17:27:59 -0300

    Implement data entry for two city cards
    Added functionality to input city data for two cards.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,63 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+//carta 1
+    char estado1;
+    char codigo1[3];
+    char cidade1[10]; 
+    int populacao1;
+    float area1;
+    float PIB1;
+    int NPT1;
+//carta 2
+    char estado2;
+    char codigo2[3];
+    char cidade2[10]; 
+    int populacao2;
+    float area2;
+    float PIB2;
+    int NPT2;
 
-  // Área para entrada de dados
+//inicio do jogo
+    printf("----INICIO DO JOGO----\n\n");
 
-  // Área para exibição dos dados da cidade
+//dados da primeira carta
+    printf("DADOS DA PRIMEIRA CARTA:\n\n");
 
-return 0;
-} 
+    printf("Digite uma letra pra representar o estado1: ");
+    scanf(" %c", &estado1);
+    printf("Digite o codigo da carta1: ");
+    scanf("%s", &codigo1);
+    printf("DIgite o nome para representar a cidade1: ");
+    scanf("%s", &cidade1);
+    printf("Digite o numero da populacao da cidade1: ");
+    scanf("%d", &populacao1);
+    printf("Digite a area da cidade1: ");
+    scanf("%f", &area1);
+    printf("Digite o NPT da cidade1: ");
+    scanf("%f", &NPT1);
+    printf("AGUARDE...\n\n");
+
+
+//dados da segunda carta
+    printf("DADOS DA SEGUNDA CARTA:\n\n");
+
+    printf("Digite uma letra pra representar o estado2: ");
+    scanf(" %c", &estado2);
+    printf("Digite o codigo da carta2: ");
+    scanf("%s", &codigo2);
+    printf("DIgite o nome para representar a cidade2: ");
+    scanf("%s", &cidade2);
+    printf("Digite o numero da populacao da cidade2: ");
+    scanf("%d", &populacao2);
+    printf("Digite a area da cidade2: ");
+    scanf("%f", &area2);
+    printf("Digite o NPT da cidade2: ");
+    scanf("%f", &NPT2);
+    
+
+    printf("FIM DE JOGO");
+
+    return 0;
+
+}
