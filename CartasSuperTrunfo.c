baf1de8748ce3619c2518eeb66d3549e284786df
KAYKISON <kaikisouza988@gmail.com>
2026-07-26 19:31:30 +0000

    fiz o codigo nivel novato, logo após, alterei e atualizei o codigo adicionando novos dados das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,28 +3,32 @@
 int main() {
 //carta 1
     char estado1;
-    char codigo1[3];
-    char cidade1[10]; 
+    char codigo1[4];
+    char cidade1[15]; 
     int populacao1;
     float area1;
     float PIB1;
     int NPT1;
+    float densidadepopulacional1;
+    float pibpercapita1;
 //carta 2
     char estado2;
-    char codigo2[3];
-    char cidade2[10]; 
+    char codigo2[4];
+    char cidade2[15]; 
     int populacao2;
     float area2;
     float PIB2;
     int NPT2;
+    float densidadepopulacional2;
+    float pibpercapita2;
 
-//inicio do jogo
-    printf("----INICIO DO JOGO----\n\n");
+    //inicio do jogo
+            printf("----INICIO DO JOGO----\n\n");
 
-//dados da primeira carta
-    printf("DADOS DA PRIMEIRA CARTA:\n\n");
+    //dados da primeira carta
+            printf("DADOS DA PRIMEIRA CARTA:\n\n");
 
-    printf("Digite uma letra pra representar o estado1: ");
+    printf("Digite uma letra para representar o estado1: ");
     scanf(" %c", &estado1);
     printf("Digite o codigo da carta1: ");
     scanf("%s", &codigo1);
@@ -34,13 +38,15 @@ int main() {
     scanf("%d", &populacao1);
     printf("Digite a area da cidade1: ");
     scanf("%f", &area1);
+    printf("Digite o PIB da cidade1: ");
+    scanf("%f", &PIB1);    
     printf("Digite o NPT da cidade1: ");
-    scanf("%f", &NPT1);
+    scanf("%d", &NPT1);
+
     printf("AGUARDE...\n\n");
 
-
-//dados da segunda carta
-    printf("DADOS DA SEGUNDA CARTA:\n\n");
+    //dados da segunda carta
+            printf("DADOS DA SEGUNDA CARTA:\n\n");
 
     printf("Digite uma letra pra representar o estado2: ");
     scanf(" %c", &estado2);
@@ -52,9 +58,40 @@ int main() {
     scanf("%d", &populacao2);
     printf("Digite a area da cidade2: ");
     scanf("%f", &area2);
+    printf("Digite o PIB da cidade2: ");
+    scanf("%f", &PIB2);
     printf("Digite o NPT da cidade2: ");
-    scanf("%f", &NPT2);
-    
+    scanf("%d", &NPT2);
+
+    //informaçoes das cartas 1 e 2
+            printf("---INFORMACOES DA CARTA1---\n\n");
+
+    printf("estado1: %c\n", estado1);
+    printf("codigo1: %s\n", codigo1);
+    printf("cidade1: %s\n", cidade1);
+    printf("populacao1: %d\n", populacao1);
+    printf("area1: %.2f\n", area1);
+    printf("PIB1: %.2f\n", PIB1);  
+    printf("NPT1: %d\n", NPT1);
+    densidadepopulacional1 = populacao1 / area1;
+    printf("densidade populacional da carta1: %.2f\n", densidadepopulacional1);
+    pibpercapita1 = PIB1 / populacao1;
+    printf("pib per capita da carta1: %.2f\n", pibpercapita1);
+
+            printf("---INFORMACOES DA CARTA2---\n\n");
+
+    printf("estado2: %c\n", estado2);
+    printf("codigo2: %s\n", codigo2);
+    printf("cidade2: %s\n", cidade2);
+    printf("populacao2: %d\n", populacao2);
+    printf("area2: %.2f\n", area2);
+    printf("PIB2: %.2f\n", PIB2);  
+    printf("NPT2: %d\n", NPT2);
+    densidadepopulacional2 = populacao2 / area2;
+    printf("densidade populacional da carta2: %.2f\n", densidadepopulacional2);
+    pibpercapita2 = PIB2 / populacao2;
+    printf("pib per capita da carta2: %.2f\n", pibpercapita2);
+
 
     printf("FIM DE JOGO");
 
